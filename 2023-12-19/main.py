c078c57bfb8eef1ac91f9ed1f513ddcfef72a507
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2023-12-19 07:38:38 +0900

    まずDIなしの簡素なやつを追加

---
+++ 2023-12-19/main.py
@@ -0,0 +1,9 @@
+def main():
+    input_text = input("please input text: ")
+    input_prefix = input("please input text which you want to add as prefix: ")
+    output = input_prefix + input_text
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
