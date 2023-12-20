0ac3e24d25ebc56edc0c9b63dfd5894c157ae581
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2023-12-21 07:42:46 +0900

    openjdkのインストールが間に合わず動作確認していないけどhelloworldとfor loop

---
+++ 2023-12-21/java/forloop.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+        System.out.println(String(i) + "hello, world¥n");
+    }
+    int i = 0;
+    for (;;) {
+        System.out.println(String(i) + "hello, world¥n");
+        i++;
+        if i < 10;
+            break
+    }
+  }
+}
