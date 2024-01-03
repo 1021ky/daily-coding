6829af63cc4b5aa333bed3169d810b02a341a55e
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-01-03 09:05:49 +0900

    基本構文としてif for switchを書いてみる

--- 2024-01-03/hello/hello.go
+++ 2024-01-03/hello/hello.go
@@ -8,4 +8,19 @@ import (
 func main() {
     fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
+	fmt.Println("following number is 2 cubed")
+	result := 1
+	for i := 0; i < 3; i++ {
+		result = result * 2
+	}
+	fmt.Println(result)
+	if result != 10 {
+		fmt.Println("previous number is not 10.")
+	}
+	switch result {
+	case 1:
+		fmt.Println("previous number is 1.")
+	case 8:
+		fmt.Println("previous number is 8.")
+	}
 }
