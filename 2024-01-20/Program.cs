09a3f0f38032c7602919d2a944b3a53e0bcb404d
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-01-20 21:33:26 +0900

    charpでfizzbuzz

---
+++ 2024-01-20/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+string FizzBuzz(int n) {
+    string result = "";
+    if (n % 3 != 0 && n % 5 != 0) {
+        return n.ToString();
+    }
+    if (n % 3 == 0) {
+        result = result + "Fizz";
+    }
+    if (n % 5 == 0) {
+        result = result + "Buzz";
+    }
+    return result;
+}
+
+for (int i=0;i<20;i++) {
+    Console.WriteLine(FizzBuzz(i+1));
+}
+
