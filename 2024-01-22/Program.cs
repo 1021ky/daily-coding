ce840ec7a59c7703ecbeef638ab37f1ebac4e187
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-01-22 07:43:37 +0900

    cshar linqをためす

---
+++ 2024-01-22/Program.cs
@@ -0,0 +1,25 @@
+﻿// LINQ practice
+int[] scores = { 97, 92, 81, 60 };
+
+// Define the query expression.
+IEnumerable<int> scoreQuery =
+    from score in scores
+    where score > 80
+    select score;
+
+// Execute the query.
+foreach (int i in scoreQuery)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+
+foreach (int score in scores)
+{
+    if (score > 80)
+    {
+        Console.Write(score + " ");
+    }
+}
+Console.WriteLine();
