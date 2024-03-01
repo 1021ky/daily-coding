49b307129bbca14e98138aec8c9a03070badd48e
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-02 08:05:01 +0900

    fsharpでfizzbuzz

---
+++ 2024-03-02/fizzbuzz/Program.fs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// fizzbuzzを標準出力する
+/// </summary>
+
+let number = 3
+
+let fizzbuzz number =
+    match number with
+    | _ when number % 3 = 0 && number % 5 = 0 -> "fizzbuzz"
+    | _ when number % 3 = 0 -> "fizz"
+    | _ when number % 5 = 0 -> "buzz"
+    | _ -> string number
+
+let numbers = [for i in 1..30 -> i]
+numbers
+|> List.map fizzbuzz
+|> List.iter (fun x  -> printfn $"{x}")
---
+++ 2024-03-02/starting/Program.fs
@@ -0,0 +1,14 @@
+﻿// 利用する型やメソッドを定義
+// ただし以降のコードでSystemの型やメソッドを使っていない
+open System
+
+// リストの定義
+let names = ["apple";"cake";"sushi"]
+// 引数foodで変数を埋め込んだ文字列を生成する関数declareFavoriteFoodを定義
+let declareFavoriteFood food = $"i like {food}"
+//　変数に対してパイプラインで処理をする
+names
+// 各要素に対して関数を実行
+|> List.map declareFavoriteFood
+// 前のパイプラインで処理された結果に対して無名関数（引数sayで引数を標準出力）
+|> List.iter (fun say -> printfn $"{say}")
