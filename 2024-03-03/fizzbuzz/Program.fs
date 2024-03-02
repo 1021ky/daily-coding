9002c7db44a1388f275b4e1a77dc84645fe29213
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-03 07:39:52 +0900

    fsharpでfizzbuzz

---
+++ 2024-03-03/fizzbuzz/Program.fs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// fizzbuzzを標準出力する
+/// </summary>
+open System
+
+let startNumber = 1
+let endNumber = 30
+let numbers = [for i in startNumber..endNumber -> i]
+
+let kiriri3 =  "( ｰ`дｰ´)"
+let kiriri5 =  "(๑•̀ㅁ•́๑)✧"
+let hogee = "_(:3」∠)_"
+
+let judgeFizzBuzz number =
+    match number with
+    | _ when number % 3 = 0 && number % 5 = 0 -> hogee + "<\t"+ string number + "〜〜"
+    | _ when number % 3 = 0  -> kiriri3 + "  <\t" + string number + "!!"
+    | _ when number % 5 = 0  ->  kiriri5 +  "  <\t" + string number + "!!!!"
+    | _ -> "(-_-)  <\t" + string number
+
+numbers
+|> List.map(judgeFizzBuzz)
+|> List.iter(fun x -> printfn $"{x}")
