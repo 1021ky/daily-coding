6d36aed82ee46f73105b0f79c1e17a43333f1339
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-06 22:11:37 +0900

    fsharpでfizzbuzz

---
+++ 2024-03-06/fizzbuzz/Program.fs
@@ -0,0 +1,19 @@
+﻿module FizzBuzz =
+    let startNumber = 1
+    let endNumber = 100
+    let numbers = [for i in startNumber..endNumber -> i]
+
+    let fizz = "fizz"
+    let buzz = "buzz"
+    let fizzbuzz = "fizzbuzz"
+
+    let judgeFizzBuzz number =
+        match number with
+        | _ when number % 3 = 0 && number % 5 = 0 -> fizzbuzz
+        | _ when number % 3 = 0 -> fizz
+        | _ when number % 5 = 0 -> buzz
+        | _ -> string number
+
+    numbers
+    |> List.map(judgeFizzBuzz)
+    |> List.iter(fun x -> printfn $"%s{x}")
