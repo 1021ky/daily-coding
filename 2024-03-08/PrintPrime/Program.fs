010bd028cae7191632ceef5b82eccde74a344796
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-08 17:46:08 +0900

    F#でもう一回素数出力をリトライ

---
+++ 2024-03-08/PrintPrime/Program.fs
@@ -0,0 +1,14 @@
+﻿// 1から100の中の素数を出力するプログラム
+
+let isPrime number =
+    if number = 0 then
+        false
+    else
+        let bound = int  (sqrt (float number))
+        // 平方根以下の値で割り切れる値が1つもなかったら素数
+        seq {2..number} |> Seq.exists (fun x -> number % x = 0) |> not
+
+[2..100]
+|> List.filter isPrime
+|> List.iter (printfn "%d")
+
