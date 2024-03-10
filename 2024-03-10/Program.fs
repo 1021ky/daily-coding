f7590cc28b30daa6e2d6e79583984f5537a9aeaf
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-10 22:53:12 +0900

    F#で素数を出力。ListとSeqの使い分けがあと少し

---
+++ 2024-03-10/Program.fs
@@ -0,0 +1,12 @@
+﻿/// <summary> 素数を出力する</summary>
+
+let isPrime number =
+    if number < 2 then
+        false
+    else
+        let root = int (sqrt(float number))
+        seq {2..root} |> Seq.exists (fun x -> number % x = 0) |> not
+
+[1..100]
+|> List.filter isPrime
+|> List.iter (printfn "%d")
