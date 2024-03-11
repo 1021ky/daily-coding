5a2a193792f08999d57e7a60e1f50cd343efda7a
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-11 22:05:33 +0900

    f#リトライ。素数出力

---
+++ 2024-03-11/PrintPrime.fsharp/Program.fs
@@ -0,0 +1,15 @@
+﻿///<summary>
+///素数を出力するプログラム
+///</summary>
+
+let isPrime number =
+    if number < 2 then
+        false
+    else
+        let root = int (sqrt(float number))
+        seq {2..root} |>  Seq.exists(
+            fun x -> number % x = 0) |> not
+[1..100]
+
+|> List.filter isPrime
+|> List.iter (printfn "{%d}")
