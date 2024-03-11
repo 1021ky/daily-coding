79b68e76417721082dd4b10ff5e2488e84101f5f
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-12 06:47:46 +0900

    FSharpで素数を出すものを空で書く。funの構文とSeqとListのリテラル定義ができればOK

---
+++ 2024-03-12/Program.fs
@@ -0,0 +1,10 @@
+﻿let isPrime number =
+    if number < 2 then
+        false
+    else
+        let root = int (sqrt(float(number)))
+        seq {2..root} |> Seq.exists(fun x -> number % x = 0) |> not
+
+[1..100]
+|> List.filter(isPrime)
+|> List.iter(printfn "%d")
