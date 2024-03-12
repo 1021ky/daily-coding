86d7fc42e11fbae5b99762777064c9846f8b505a
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-13 07:58:31 +0900

    ようやくF#で素数を出すプログラムを空で書けた

---
+++ 2024-03-13/Program.fs
@@ -0,0 +1,13 @@
+﻿/// <summary>素数を出力するプログラム</summary>
+
+// 素数判定関数
+let isPrime number =
+    if number < 2 then
+        false
+    else
+        let root = int (sqrt (float number))
+        {2..root} |> Seq.exists(fun x -> number % x = 0) |> not
+
+[1..100]
+|> List.filter(isPrime)
+|> List.iter(printfn "%d")
