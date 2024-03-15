87a991af737b65c04829b3c868dde63b6d636ba1
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-03-15 21:51:02 +0900

    F#で素数を出力。空で書けた！

---
+++ 2024-03-15/Prime/Program.fs
@@ -0,0 +1,14 @@
+﻿/// <summary>1から100のうち素数を出力するプログラム</summary>
+
+let isPrime number =
+    if number < 2 then
+        false
+    else
+        let root = int(sqrt( float number))
+        {2..root} |> Seq.exists(fun x -> number % x = 0) |> not
+
+
+let numbers = [1..100]
+numbers
+|> List.filter(isPrime)
+|> List.iter(fun x -> printfn $"{x}")
