fcae586dfb831db7a38d4ea149aabda36312a605
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-05-06 11:42:28 +0900

    F#でfizzbuzzを書く

---
+++ 2024-05-06/Program.fs
@@ -0,0 +1,12 @@
+﻿/// <symmary>fizzbuzz</summary>
+
+let determine_fizz_buzz number =
+    match number with
+    | _ when number % 15 = 0 -> "FizzBuzz"
+    | _ when number % 5 = 0 -> "Buzz"
+    | _ when number % 3 = 0 -> "Fizz"
+    | _ -> string number
+
+[1..100]
+|> List.map(determine_fizz_buzz) // mapなので新しいリストが返る
+|> List.iter(printfn "%s") // iterなので結果は返さない
