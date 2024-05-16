d825e59e642279886dbcb59ff1ac8566080e411e
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-05-16 17:47:56 +0900

    f#で行数をカウントする

---
+++ 2024-05-16/Program.fs
@@ -0,0 +1,9 @@
+﻿// wc
+
+let args = System.Environment.GetCommandLineArgs()
+
+let filePath = args[1]
+
+let fileLines = System.IO.File.ReadAllLines(filePath)
+let arrayLength = Array.length(fileLines)
+printfn "lines %d" arrayLength
