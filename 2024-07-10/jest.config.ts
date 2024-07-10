0de44f004eae4fefbbe1d0581d5d1f55300c403c
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-07-10 21:27:18 +0900

    typescript+jestで単体テストコードを書く

---
+++ 2024-07-10/jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  "roots": [
+    "."
+  ],
+  "testMatch": [
+    "src/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  "moduleNameMapper": {
+    "^@App/(.*)$": "<rootDir>/src/$1"
+  },
+}
