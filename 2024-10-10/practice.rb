02af38e0c5b2ea961327aecf85e79988fc000abe
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-10-10 22:05:01 +0900

    まだ良くわからない。Syntax Errorになるのはなぜか？引き続き要練習

--- 2024-10-10/practice.rb
+++ 2024-10-10/practice.rb
@@ -2,21 +2,38 @@
 # 問題 1:
 # ハッシュのキーとしてシンボルを使い、3つの情報（名前、年齢、職業）を含むハッシュを返すメソッド `create_profile` を作成してください。
 # 各キーはシンボルで、例えば :name, :age, :job のように指定してください。
-def create_profile
-  # ここにコードを記述
+def create_profile(name, age, job)
+  {:name => name, :age => age, :job => job}
 end
 
-# 問題 2:
-# 引数としてシンボルを受け取り、そのシンボルがハッシュのキーとして存在するか確認し、
-# 存在すれば対応する値を返し、存在しなければ "Not found" を返すメソッド `find_in_hash` を作成してください。
-# ハッシュは {name: "Bob", age: 25} を使い、シンボルを引数に取ります。
-def find_in_hash(symbol)
-  # ここにコードを記述
-end
+# これはできない。キーには任意の種類のオブジェクトを用いることができるが、hashとeql?が定義されていないとだめなので
+# p {'name' => 'fushiguro', 'age' => 16, 'job' => 'jujutsushi'}
+# これは可能。シンボルは上記メソッドが定義されている
+# p {:name => 'fushiguro', :age => 123, :job => 'jujutsushi'}
 
-# 問題 3:
-# 引数として文字列を受け取り、その文字列をシンボルに変換して返すメソッド `string_to_symbol` を作成してください。
-# 例えば、引数 "hello" を渡すとシンボル :hello を返すようにしてください。
-def string_to_symbol(str)
-  # ここにコードを記述
-end
+# irb(main):006> :name.methods.include?(:eql?)
+# => true
+# irb(main):007> :name.methods.include?(:hash)
+# => true
+# irb(main):008>
+# キーがシンボルに限り省略できる
+# p {name: 'fushiguro', age: 16, job: 'jujutsushi'}
+
+result = create_profile("yuji_itadori", 16, "jujutsushi")
+p result[:name]
+
+# # 問題 2:
+# # 引数としてシンボルを受け取り、そのシンボルがハッシュのキーとして存在するか確認し、
+# # 存在すれば対応する値を返し、存在しなければ "Not found" を返すメソッド `find_in_hash` を作成してください。
+# # ハッシュは {name: "Bob", age: 25} を使い、シンボルを引数に取ります。
+# def find_in_hash(symbol)
+#   data = {name: "Bob", age: 25}
+#   data.
+# end
+
+# # 問題 3:
+# # 引数として文字列を受け取り、その文字列をシンボルに変換して返すメソッド `string_to_symbol` を作成してください。
+# # 例えば、引数 "hello" を渡すとシンボル :hello を返すようにしてください。
+# def string_to_symbol(str)
+#   # ここにコードを記述
+# end
