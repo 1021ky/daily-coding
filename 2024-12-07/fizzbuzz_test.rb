79b5d276d40aac46ee89580fc88ec51bcfd09a44
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-12-07 06:34:15 +0900

    minitest unittestを使ってTDD

---
+++ 2024-12-07/fizzbuzz_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require './fizzbuzz.rb'
+
+class FizzBuzzTest < Minitest::Test
+    def test_fizzbuzz
+        [
+            {
+                'testcase': '3で割り切れるときはfizz',
+                'param': '3',
+                'expected': 'fizz'
+            },
+            {
+                'testcase': '5で割り切れるときはbuzz',
+                'param': '5',
+                'expected': 'buzz'
+            },
+            {
+                'testcase': '15で割り切れるときはfizzbuzz',
+                'param': '15',
+                'expected': 'fizzbuzz'
+            },
+            {
+                'testcase': '3,5,15で割り切れないときはそのまま出力する',
+                'param': '4',
+                'expected': '4'
+            }
+        ].each do |params|
+            puts "testcase: #{params[:testcase]}"
+            # Arrange
+            arg = params[:param]
+            expected = params[:expected]
+            # Act
+            actual = fizzbuzz(arg)
+            assert_equal(expected, actual)
+        end
+    end
+end
