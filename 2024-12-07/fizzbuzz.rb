66c27aeff2eec5403d8b2479f63c71f12f2f3748
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-12-10 08:16:18 +0900

    minitestを試す

--- 2024-12-07/fizzbuzz.rb
+++ 2024-12-07/fizzbuzz.rb
@@ -1,3 +1,12 @@
-def fizzbuzz(n)
-    n
+def fizzbuzz(value)
+    case
+    when value%15==0
+        'fizzbuzz'
+    when value%3==0
+        'fizz'
+    when value%5==0
+        'buzz'
+    else
+        value.to_s
+    end
 end
--- 2024-12-07/fizzbuzz_test.rb
+++ 2024-12-07/fizzbuzz_test.rb
@@ -5,32 +5,32 @@ class FizzBuzzTest < Minitest::Test
     def test_fizzbuzz
         [
             {
-                'testcase': '3で割り切れるときはfizz',
-                'param': '3',
-                'expected': 'fizz'
+                testcase: '3で割り切れるときはfizz',
+                param: 3,
+                expected: 'fizz'
             },
             {
-                'testcase': '5で割り切れるときはbuzz',
-                'param': '5',
-                'expected': 'buzz'
+                testcase: '5で割り切れるときはbuzz',
+                param: 5,
+                expected: 'buzz'
             },
             {
-                'testcase': '15で割り切れるときはfizzbuzz',
-                'param': '15',
-                'expected': 'fizzbuzz'
+                testcase: '15で割り切れるときはfizzbuzz',
+                param: 15,
+                expected: 'fizzbuzz'
             },
             {
-                'testcase': '3,5,15で割り切れないときはそのまま出力する',
-                'param': '4',
-                'expected': '4'
+                testcase: '3,5,15で割り切れないときはそのまま出力する',
+                param: 4,
+                expected: '4'
             }
         ].each do |params|
-            puts "testcase: #{params[:testcase]}"
             # Arrange
-            arg = params[:param]
+            param = params[:param]
             expected = params[:expected]
             # Act
-            actual = fizzbuzz(arg)
+            actual = fizzbuzz(param)
+
             assert_equal(expected, actual)
         end
     end
