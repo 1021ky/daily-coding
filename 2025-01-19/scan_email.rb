df6d94197472b7c2baa8f7d74bafe3478c335578
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-01-20 20:29:46 +0900

    rubocop

--- 2025-01-19/scan_email.rb
+++ 2025-01-19/scan_email.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 def scan_email(text)
-  rexp = /\b[\w._%+-]+@[[\w-]+\.]{2,}[\w]{2,}\b/i
+  rexp = /\b[\w._%+-]+@[[\w-]+.]{2,}\w{2,}\b/i
   res = text.scan(rexp)
   return [] if res.empty?
+
   res
 end
