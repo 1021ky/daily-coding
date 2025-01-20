383c175312bb9c7c4a7568df2cae7e33c2b32026
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-01-20 19:51:51 +0900

    \sと\wを間違えていたため修正

--- 2025-01-19/scan_email.rb
+++ 2025-01-19/scan_email.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-if __FILE__ == $PROGRAM_NAME
-  text = <<~TEXT
-    お問い合わせはこちら: support@example.com
-    担当者: tanaka.taro@company.co.jp
-    チームアドレス: team@project.org
-    無効な例: example@com, @missingprefix.com, invalid@@domain.com
-  TEXT
-  scan_email
-  scan_user
+def scan_email(text)
+  rexp = /\b[\w._%+-]+@[[\w-]+\.]{2,}[\w]{2,}\b/i
+  res = text.scan(rexp)
+  return [] if res.empty?
+  res
 end
--- 2025-01-19/scan_email_test.rb
+++ 2025-01-19/scan_email_test.rb
@@ -12,7 +12,14 @@ class ScanEmailTest < Minitest::Test
       無効な例: example@com, @missingprefix.com, invalid@@domain.com
     TEXT
     actual = scan_email(text)
-    expected = ['support@example.com', 'tanaka.taro@company.co.jp']
-    assert_equal(expected.sort!, actual.sort!)
+    expected = ['support@example.com', 'tanaka.taro@company.co.jp', 'team@project.org']
+    assert_equal expected.sort, actual.sort
+  end
+
+  def test_scan_email_empty_str
+    text = ''
+    actual = scan_email(text)
+    expected = []
+    assert_equal expected.sort, actual.sort
   end
 end
