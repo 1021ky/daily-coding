bfa21229c200db4f110bca008cc02f3a21b4fa35
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-01-20 20:29:23 +0900

    パラメータ化テストとしてわかりやすくする

--- 2025-01-19/scan_email_test.rb
+++ 2025-01-19/scan_email_test.rb
@@ -3,23 +3,30 @@
 require 'minitest/autorun'
 require './scan_email'
 
-class ScanEmailTest < Minitest::Test
-  def test_scan_email
-    text = <<~TEXT
-      お問い合わせはこちら: support@example.com
-      担当者: tanaka.taro@company.co.jp
-      チームアドレス: team@project.org
-      無効な例: example@com, @missingprefix.com, invalid@@domain.com
-    TEXT
-    actual = scan_email(text)
-    expected = ['support@example.com', 'tanaka.taro@company.co.jp', 'team@project.org']
-    assert_equal expected.sort, actual.sort
-  end
-
-  def test_scan_email_empty_str
-    text = ''
-    actual = scan_email(text)
-    expected = []
-    assert_equal expected.sort, actual.sort
+describe 'ScanEmail' do
+  [{
+    description: 'valid_emails',
+    input: {
+      text: <<~TEXT,
+        お問い合わせはこちら: support@example.com
+        担当者: tanaka.taro@company.co.jp
+        チームアドレス: team@project.org
+        無効な例: example@com, @missingprefix.com, invalid@@domain.com
+      TEXT
+      expected: ['support@example.com', 'tanaka.taro@company.co.jp', 'team@project.org']
+    }
+  },
+   {
+     description: 'empty_string',
+     input: {
+       text: '',
+       expected: []
+     }
+   }].each do |test_case|
+    it "scan_email: #{test_case[:description]}" do
+      input = test_case[:input]
+      actual = scan_email(input[:text])
+      assert_equal input[:expected].sort, actual.sort
+    end
   end
 end
