216471b59e33d1988bdae1bc671ecc46d05c4178
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-02-11 23:55:43 +0900

    回文判定をする

---
+++ 2025-02-11/palindrome.rb
@@ -0,0 +1,10 @@
+#
+# 回文か判定する
+#
+# @param [String] target 判定対象
+#
+# @return [Bool] 回分ならばTrue
+#
+def palindrome? target
+  target == target.reverse
+end
---
+++ 2025-02-11/palindrome_spec.rb
@@ -0,0 +1,24 @@
+require 'minitest/autoload'
+
+require './palindrome'
+
+describe "palindrome" do
+    [
+      { ctx: '回文となる単語が渡されたとき',
+        target: "Racecar",
+        expected: true },
+      { ctx: '大文字・小文字を含んだ回文が渡されたとき',
+        target: "Madam, in Eden, I'm Adam.",
+        expected: true },
+      { ctx: '記号を含む回文が渡されたとき',
+        target: "Was it a car or a cat I saw?",
+        expected: true },
+      { ctx: '回文ではない文が渡されたとき',
+        target: "It is a cat",
+        expected: false },
+    ].each do |test_case|
+      it (test_case[:ctx]).to_s do
+        expect(palindrome?(test_case[:target])).to_be test_case[:expected]
+      end
+    end
+end
