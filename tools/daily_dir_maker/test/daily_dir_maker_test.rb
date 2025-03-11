c146acd26bcf2f6422fab22455e3083df0599e6f
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-03-12 08:23:34 +0900

    テストでstubとクリーンアップ処理をいれて毎回同じ条件で実行できるようにする

--- tools/daily_dir_maker/test/daily_dir_maker_test.rb
+++ tools/daily_dir_maker/test/daily_dir_maker_test.rb
@@ -6,16 +6,18 @@ require_relative "test_helper"
 describe DailyDirMaker do
 
   describe "when there is no directory" do
+    after 'clean dir' do
+      Dir.delete '2025-03-12' if Dir.exist? '2025-03-12'
+    end
     it "daily directory is to be make" do
       expected = Time.now.strftime('%Y-%m-%d')
 
-      DailyDirMaker.make
+      Time.stub(:now, Time.new(2025, 3, 12)) do
+        DailyDirMaker.make
+      end
 
       dirs = Dir.children('.')
-
       _(dirs).must_include expected
-
-      Dir.delete(expected)
     end
   end
 end
