590f9d7dbc8337a781a2b81f58ff045c0695f32f
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-03-16 00:08:15 +0900

    rubyでStrategyパターンを書く

--- 2025-03-15/payment.rb
+++ 2025-03-15/payment.rb
@@ -1,20 +1,43 @@
-# 練習としてリファクタリングする
+class CreditCardPayment
+  def pay price
+    p "use credit card, $#{price}"
+  end
+end
+
+class CashPayment
+  def pay price
+    p "use cash, $#{price}"
+  end
+end
+
+class PayPayPayment
+  def pay price
+    p "use cash', $#{price}"
+  end
+end
+
+STRATEGIES = {
+  'credit_card': CreditCardPayment.new,
+  'cash': CashPayment.new,
+  'paypay': PayPayPayment.new
+}
+def decide_payment_strategy method
+  STRATEGIES[method.to_sym]
+end
 
 #
 # 支払い方法に応じて処理をする
 #
-# @param [<Type>] method <description>
-# @param [<Type>] price <description>
-#
-# @return [<Type>] <description>
+# @param [String] method 支払い方法
+# @param [Integer] price 価格
 #
 def decide_payment_method(method:, price:)
-  case method
-  when 'credit_card'
-    credit_card.pay(price)
-  when 'cash'
-    cash.pay(price)
-  when 'paypay'
-    paypay.pay(price)
-  end
+  strategy = decide_payment_strategy method
+  strategy.pay(price)
+end
+
+if __FILE__ == $PROGRAM_NAME
+  decide_payment_method(method: 'credit_card', price: 100)
+  decide_payment_method(method: 'cash', price: 50)
+  decide_payment_method(method: 'paypay', price: 75)
 end
