645659b6abfcb9539acace37ee14796fc9bd8f87
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-07-11 23:19:08 +0900

    JS非同期処理についてためす

---
+++ 2025-07-11/index.js
@@ -0,0 +1,16 @@
+/* 非同期処理 */
+function taskA() {
+    console.log("タスクAを実行 at " + Date.now());
+}
+function taskB() {
+    console.log("タスクBを実行 at " + Date.now());
+}
+function taskAsync() {
+    console.log("非同期のタスクを実行 at " + Date.now());
+}
+
+taskA();
+setTimeout(() => {
+    taskAsync();
+}, 1000);
+taskB();
