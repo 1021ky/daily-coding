6084f7b6b8ee281f1ce2575679348973d5e4f684
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-08-09 16:38:51 +0900

    HTML文字列をエスケープしてから表示する

--- 2025-08-09/ajaxapp/index.js
+++ 2025-08-09/ajaxapp/index.js
@@ -1,4 +1,8 @@
 let userInfo = null;
+/**
+ * 指定されたユーザーIDに基づいてGitHubユーザー情報を取得する
+ * @param {*} userId
+ */
 function fetchUserInfo(userId) {
     fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
         .then(response => {
@@ -23,8 +27,48 @@ function fetchUserInfo(userId) {
         });
 }
 
+/**
+ * エスケープ文字列を生成する
+ *
+ * XSS防止に使用する。
+ *
+ * @param {*} str
+ * @returns エスケープされた文字列
+ */
+function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+/**
+ * 指定された文字列と値をHTMLエスケープして結合する
+ *
+ * タグ付きテンプレート関数として使用する。
+ *
+ * @param {*} strings ${}で区切られた文字列の配列
+ * @param  {...any} values ${}の評価値の配列
+ * @returns HTMLエスケープされた文字列
+ */
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, str, i) => {
+        const value = values[i - 1]; // タグ付きテンプレートでは、必ず区切られた文字列の数は評価値の数より1つ多いため、i-1で取得する
+        if (typeof value === "string") {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    });
+}
+
+/**
+ * ユーザー情報を表示する
+ */
 function displayUserInfo() {
-    const view = `
+    const view = escapeHTML`
             <h4>${userInfo.name} (@${userInfo.login})</h4>
             <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
             <dl>
@@ -32,8 +76,8 @@ function displayUserInfo() {
                 <dd>${userInfo.location}</dd>
                 <dt>Repositories</dt>
                 <dd>${userInfo.public_repos}</dd>
-                    </dl>
-                    `;
+            </dl>
+        `;
 
     const result = document.getElementById("result");
     result.innerHTML = view;
