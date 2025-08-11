2f2cad8b5d0156937cbf7192d4fb725bae16b231
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-08-11 18:29:15 +0900

    入力されたTODOを一覧に追加できるようにする

--- 2025-08-11/src/App.js
+++ 2025-08-11/src/App.js
@@ -1,4 +1,5 @@
-console.log("App.js: loaded");
+import { element, render } from "./view/html-util.js";
+
 export class App {
     constructor() {
         console.log("App initialized");
@@ -7,9 +8,26 @@ export class App {
     mount() {
         const formElement = document.querySelector("#js-form");
         const inputElement = document.querySelector("#js-form-input");
+        const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        // TodoリストをまとめるList要素
+        const todoListElement = element`<ul></ul>`;
+        // Todoアイテム数
+        let todoItemCount = 0;
         formElement.addEventListener("submit", (event) => {
-            event.preventDefault(); // デフォルトの動作、ここでは押下したら別のページに遷移する、を止める
-            console.log(`入力欄の値: ${inputElement.value}`);
+            // 本来のsubmitイベントの動作を止める
+            event.preventDefault();
+            // 追加するTodoアイテムの要素(li要素)を作成する
+            const todoItemElement = element`<li>${inputElement.value}</li>`;
+            // TodoアイテムをtodoListElementに追加する
+            todoListElement.appendChild(todoItemElement);
+            // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
+            render(todoListElement, containerElement);
+            // Todoアイテム数を+1し、表示されてるテキストを更新する
+            todoItemCount += 1;
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            // 入力欄を空文字列にしてリセットする
+            inputElement.value = "";
         });
     }
 }
