0170278060a186c51be078c476f709125ba0e88e
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-10-18 20:15:40 +0900

    formオブジェクトとなるRequestクラスを追加する

--- 2025-10-18/zip_lookup/app/controllers/zip_codes_controller.rb
+++ 2025-10-18/zip_lookup/app/controllers/zip_codes_controller.rb
@@ -1,18 +1,12 @@
 class ZipCodesController < ApplicationController
   def show
-    zip_code = ZipCode.includes(:zip_code_address, :zip_code_address_furigana).find_by(zipcode: params[:zip_code])
+    request = ZipCodeRequest.from(params)
+    code = request.verify!
+    zip_code = ZipCode.includes(:zip_code_address, :zip_code_address_furigana).find_by(zipcode: code)
     return head :not_found unless zip_code
 
     render json: ZipCodeSerializer.new(zip_code).serializable_hash
-  end
-
-  private
-
-  def zip_code_param
-    params.require(:zip_code)
-  end
-
-  def render_bad_request(_error = nil)
-    render json: { message: '郵便番号の指定に誤りがあります' }, status: :bad_request
+  rescue ZipCodeRequest::InvalidRequestError => e
+    render json: { message: e.message }, status: :bad_request
   end
 end
---
+++ 2025-10-18/zip_lookup/app/models/zip_code_request.rb
@@ -0,0 +1,25 @@
+class ZipCodeRequest
+  include ActiveModel::Model
+
+  InvalidRequestError = Class.new(StandardError)
+
+  attr_accessor :zip_code
+
+  validates :zip_code, presence: true, format: { with: /\A\d{7}\z/ }
+
+  def self.from(params)
+    new(zip_code: params.require(:zip_code))
+  rescue ActionController::ParameterMissing
+    new
+  end
+
+  def verify!
+    return zip_code if valid?
+
+    raise InvalidRequestError, '郵便番号の指定に誤りがあります'
+  end
+
+  def zip_code=(value)
+    @zip_code = value.presence&.to_s
+  end
+end
