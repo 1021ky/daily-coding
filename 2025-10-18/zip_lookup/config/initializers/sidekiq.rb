7ec19498ac8514a8acedc089e4115a826848b28e
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-10-18 17:00:59 +0900

    ローカルでは環境変数なしでもsidekiqが動くようにする

--- 2025-10-18/zip_lookup/config/initializers/sidekiq.rb
+++ 2025-10-18/zip_lookup/config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # Sidekiq内部で扱うデータ用redisの設定
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis:6379/0') }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
 end
 
 # キュー管理用redisの設定
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis:6379/0') }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
 end
