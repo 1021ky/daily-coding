1b6e7218f3656c0cb7fbff242cfbeb217f9fbb48
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-10-18 19:47:09 +0900

    ストロングパラメータでリクエストのチェックをする

--- 2025-10-18/zip_lookup/app/controllers/zip_codes_controller.rb
+++ 2025-10-18/zip_lookup/app/controllers/zip_codes_controller.rb
@@ -1,8 +1,18 @@
 class ZipCodesController < ApplicationController
   def show
-    zip_code = ZipCode.includes(:zip_code_address, :zip_code_address_furigana).find_by(zipcode: params[:code])
+    zip_code = ZipCode.includes(:zip_code_address, :zip_code_address_furigana).find_by(zipcode: params[:zip_code])
     return head :not_found unless zip_code
 
     render json: ZipCodeSerializer.new(zip_code).serializable_hash
+  end
+
+  private
+
+  def zip_code_param
+    params.require(:zip_code)
+  end
+
+  def render_bad_request(_error = nil)
+    render json: { message: '郵便番号の指定に誤りがあります' }, status: :bad_request
   end
 end
