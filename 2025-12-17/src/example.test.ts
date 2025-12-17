2cf19f0c158994c70f37aa01271735308d68156c
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-12-17 22:52:55 +0900

    時間を決めてクイックソートリトライ

--- 2025-12-17/src/example.test.ts
+++ 2025-12-17/src/example.test.ts
@@ -1,8 +1,15 @@
 import { expect, test } from "vitest";
-import run from "@/example";
-test.each([{ expected: "sample" }])(
-  "sample() -> $expected",
-  ({ expected }: { expected: string }) => {
-    expect(run()).toBe(expected);
+import sort from "@/example";
+test.each([
+  { param: [1, 2, 3, 4, 5], expected: [1, 2, 3, 4, 5] },
+  { param: [2, 2, 3, 3, 3, 2, 2], expected: [2, 2, 2, 2, 3, 3, 3] },
+  { param: [4, 1, 2, 0, 2, 1, 4, 5, 3], expected: [0, 1, 1, 2, 2, 3, 4, 4, 5] },
+  { param: [], expected: [] },
+])(
+  "sample($param) -> $expected",
+  ({ param, expected }: { param: number[]; expected: number[] }) => {
+    sort(param);
+    const actual = param;
+    expect(actual).toEqual(expected);
   },
 );
--- 2025-12-17/src/example.ts
+++ 2025-12-17/src/example.ts
@@ -1,3 +1,27 @@
-export default function run(): string {
-  return "sample";
+export default function sort(
+  target: number[],
+  left_index: number = 0,
+  right_index: number = target.length - 1,
+): void {
+  // 要素が1しかないならばなにもせずおえる
+  if (left_index >= right_index) return;
+  // ピボットをとる
+  const pivot = target[Math.floor((left_index + right_index) / 2)];
+  // よせる
+  let left_i = left_index;
+  let right_i = right_index;
+  while (left_i <= right_i) {
+    while (target[left_i] < pivot) left_i++;
+    while (target[right_i] > pivot) right_i--;
+    if (left_i <= right_i) {
+      const tmp = target[left_i];
+      target[left_i] = target[right_i];
+      target[right_i] = tmp;
+      left_i++;
+      right_i--;
+    }
+  }
+  if (left_index < left_i) sort(target, left_index, right_i);
+  if (right_i < right_index) sort(target, left_i, right_index);
+  return;
 }
